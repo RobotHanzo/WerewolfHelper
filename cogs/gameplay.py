3698d892b0ad710cd62fb0a7ef3ba547a195d79f
RobotHanzo <nathanlee302@outlook.com>
2021-07-17 15:13:24 +0800

    debug

--- cogs/gameplay.py
+++ cogs/gameplay.py
@@ -27,6 +27,8 @@ class Gameplay(commands.Cog):
                              components=[create_actionrow(create_button(ButtonStyle.red, label='結束計時'))])
         try:
             def check(comp_ctx: ComponentContext):
+                print(ctx.author.id == comp_ctx.author.id)
+                print(ctx.message.id == comp_ctx.message.id)
                 return ctx.author.id == comp_ctx.author.id & ctx.message.id == comp_ctx.message.id
 
             await self.bot.wait_for('component', check=check, timeout=interval)
