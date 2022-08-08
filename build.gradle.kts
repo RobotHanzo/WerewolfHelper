e91ac1b6bce5ba863a34656d72ad658005f26381
RobotHanzo <RobotHanzo@users.noreply.github.com>
2022-08-08 14:30:41 +0800

    Migrated to Gradle Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    java
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+group = "dev.robothanzo.werewolf"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://jitpack.io")
+    maven("https://m2.dv8tion.net/releases")
+}
+
+dependencies {
+    implementation("net.dv8tion:JDA:5.0.0-alpha.16")
+    implementation("club.minnced:discord-webhooks:0.8.0")
+    implementation("org.mongodb:mongo-java-driver:3.12.11")
+    implementation("ch.qos.logback:logback-classic:1.2.11")
+    implementation("io.github.cdimascio:dotenv-java:2.2.4")
+    implementation("com.github.RobotHanzo:JDAInteractions:v0.0.10")
+    implementation("com.sedmelluq:lavaplayer:1.3.78")
+    compileOnly("org.projectlombok:lombok:1.18.24")
+    annotationProcessor("org.projectlombok:lombok:1.18.24")
+}
+
+tasks {
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(17)
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+
+    named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+        archiveClassifier.set("")
+        minimize()
+    }
+
+    jar {
+        manifest {
+            attributes["Main-Class"] = "dev.robothanzo.werewolf.WerewolfHelper"
+        }
+    }
+}
